02d1b813f4772e78ee2ad1d861fbb95244f4374d
jognsmith <jogn.smith@riseup.net>
2013-05-30 08:31:48 +1000

    Now you can running install Checking .pth file support in /usr/local/lib/python2.7/dist-packages/ it.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+if __name__ != "__main__":
+    import sys
+    sys.exit(1)
+
+def long_desc(ext="md"):
+    # Markdown because I CBF to do reST.
+    with open('README.{0}'.format(ext), 'rb') as f:
+        return f.read()
+
+kw = {
+    "name": "blc.py",
+    "version": "1.0.0",
+    "description": "An easy way to interact with the BlooCoin environment.",
+    "long_description": long_desc(),
+    "url": "https://github.com/jognsmith/blc.py",
+    "author": "John Smith",
+    "author_email": "jogn.smith@riseup.net",
+    "license": "MIT",
+    "packages": ["blcpy"],
+    "zip_safe": False,
+    "keywords": "bloocoin wrapper client",
+    "classifiers": [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Text Processing"
+    ]
+}
+
+if __name__ == "__main__":
+    setup(**kw)
